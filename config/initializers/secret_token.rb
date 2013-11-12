bc0962adf149150469f583358cb917fee5519a3a
Raghu Betina <rbetina@gmail.com>
2013-11-12 12:37:49 -0600

    rails new hub51

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hub51::Application.config.secret_key_base = '18f67e02f74d1eb79ed007ddf5a7ce0f452aa0e797e58fd07eea1a590064340f1680684a90dafe9b06e0c20c23941c3b75f2929532404eebe4b396074d0732c2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Hub51::Application.config.session_store :cookie_store, key: '_hub51_session'
